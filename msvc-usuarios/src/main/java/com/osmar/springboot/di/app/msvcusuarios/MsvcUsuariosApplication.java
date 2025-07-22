eeaa0dfa7a67714db319b09cfc912e9bf31aabd0
Osmar Lopez Islas <osmichan07@gmail.com>
2025-07-22 11:18:26 -0600

    First configuration microservice users

---
+++ msvc-usuarios/src/main/java/com/osmar/springboot/di/app/msvcusuarios/MsvcUsuariosApplication.java
@@ -0,0 +1,13 @@
+package com.osmar.springboot.di.app.msvcusuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcUsuariosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsvcUsuariosApplication.class, args);
+    }
+
+}
---
+++ msvc-usuarios/src/test/java/com/osmar/springboot/di/app/msvcusuarios/MsvcUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.osmar.springboot.di.app.msvcusuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcUsuariosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
