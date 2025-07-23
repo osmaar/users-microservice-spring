6b7889495f01b10171ae1a6d45d2102d19d0caf5
Osmar Lopez Islas <osmichan07@gmail.com>
2025-07-22 18:11:47 -0600

    Add CursoController for managing course operations

---
+++ msvc-cursos/src/main/java/com/osmar/springboot/msvc/cursos/repositories/CursoRepository.java
@@ -0,0 +1,10 @@
+package com.osmar.springboot.msvc.cursos.repositories;
+
+import com.osmar.springboot.msvc.cursos.models.entity.Curso;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CursoRepository extends CrudRepository<Curso, Long> {
+
+
+
+}
