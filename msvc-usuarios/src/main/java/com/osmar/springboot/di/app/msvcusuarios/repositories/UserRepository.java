a86bfb4f64fc1070b148b53de679c2ee7b2e0f73
Osmar Lopez Islas <osmichan07@gmail.com>
2025-07-24 13:30:01 -0600

    Add findByEmail method to UserRepository for email-based user retrieval

--- msvc-usuarios/src/main/java/com/osmar/springboot/di/app/msvcusuarios/repositories/UserRepository.java
+++ msvc-usuarios/src/main/java/com/osmar/springboot/di/app/msvcusuarios/repositories/UserRepository.java
@@ -3,7 +3,10 @@ package com.osmar.springboot.di.app.msvcusuarios.repositories;
 import com.osmar.springboot.di.app.msvcusuarios.models.entity.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends CrudRepository <User, Long> {
 
+    Optional<User> findByEmail(String email);
 
 }
