8569dcff08a6e84176cedb40174f61ebde3e8068
Osmar Lopez Islas <osmichan07@gmail.com>
2025-07-24 13:28:36 -0600

    Update MySQL datasource password in application properties

--- msvc-cursos/src/main/java/com/osmar/springboot/msvc/cursos/repositories/CursoRepository.java
+++ msvc-cursos/src/main/java/com/osmar/springboot/msvc/cursos/repositories/CursoRepository.java
@@ -1,10 +1,13 @@
 package com.osmar.springboot.msvc.cursos.repositories;
 
 import com.osmar.springboot.msvc.cursos.models.entity.Curso;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface CursoRepository extends CrudRepository<Curso, Long> {
 
-
-
+    @Modifying
+    @Query("DELETE FROM CursoUser cu WHERE cu.userId=?1")
+    void deleteCursoUserById(Long id);
 }
