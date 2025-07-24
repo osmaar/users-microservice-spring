b765182a6cb87ef884d3773c6be0e89d67d736d9
Osmar Lopez Islas <osmichan07@gmail.com>
2025-07-24 13:28:23 -0600

    Update MySQL datasource password in application properties

--- msvc-cursos/src/main/java/com/osmar/springboot/msvc/cursos/models/entity/Curso.java
+++ msvc-cursos/src/main/java/com/osmar/springboot/msvc/cursos/models/entity/Curso.java
@@ -1,6 +1,10 @@
 package com.osmar.springboot.msvc.cursos.models.entity;
 
+import com.osmar.springboot.msvc.cursos.models.User;
 import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "cursos")
@@ -10,6 +14,21 @@ public class Curso {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nombre;
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "curso_id", nullable = false)
+    private List<CursoUser> cursosUsers;
+
+    @Transient
+    private List<User> users;
+
+    /**
+     * Constructor por defecto
+     */
+    public Curso() {
+        cursosUsers = new ArrayList<>();
+        users = new ArrayList<>();
+    }
 
     /**
      * get field @Id
@@ -53,4 +72,64 @@ public class Curso {
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
+
+    /**
+     * get field @Transient
+     *
+     * @return users @Transient
+
+     */
+    public List<User> getUsers() {
+        return this.users;
+    }
+
+    /**
+     * set field @Transient
+     *
+     * @param users @Transient
+
+     */
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+
+    /**
+     * get field @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+     *
+     * @return cursosUsers @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+
+     */
+    public List<CursoUser> getCursosUsers() {
+        return this.cursosUsers;
+    }
+
+    /**
+     * set field @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+     *
+     * @param cursosUsers @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+
+     */
+    public void setCursosUsers(List<CursoUser> cursosUsers) {
+        this.cursosUsers = cursosUsers;
+    }
+
+    /**
+     * Adds a CursoUser to the list of CursoUsers.
+     *
+     * @param cursoUser the CursoUser to add
+     */
+    public void addCursoUser(CursoUser cursoUser) {
+        this.cursosUsers.add(cursoUser);
+    }
+
+    /**
+     * Removes a CursoUser from the list of CursoUsers.
+     *
+     * @param cursoUser the CursoUser to remove
+     */
+    public void removeCursoUser(CursoUser cursoUser) {
+        this.cursosUsers.remove(cursoUser);
+    }
+
+
 }
